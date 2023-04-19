a34c3be31c30b43deda044b7e1fe08614dfe29d3
Dutt Mistry <dutta.mistry@gmail.com>
2023-04-19 15:49:35 +0530

    Done

--- src/todo/todo.js
+++ src/todo/todo.js
@@ -1,116 +1,120 @@
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import {useFormik} from 'formik';
-import React, { useEffect,useRef } from "react";
-// import {Registrationschema} from '../schemas';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import ListItem from "@mui/material/ListItem";
+import List from "@mui/material/List";
+import ListItemSecondaryAction from "@mui/material/ListItemSecondaryAction";
+import Typography from "@mui/material/Typography";
+import ListItemText from "@mui/material/ListItemText";
+import IconButton from "@mui/material/IconButton";
 
-const initialvalues ={
-  name:'',
-}
 
-export default function Todo(){
-    let Todo_items
-    let Todo_items_ref=useRef([])
-    useEffect(() => {
-        if(!localStorage.getItem('TodoList')){
-        localStorage.setItem("TodoList",JSON.stringify([]));
-        }else{
-            Todo_items_ref.current = JSON.parse(localStorage.getItem('TodoList'));
-            console.log("🚀 ~ file: todolist.js:35 ~ Todolist ~ ̥:", Todo_items_ref.current)
-            document.getElementById('todo_items').innerHTML= Todo_items_ref.current;
-            // Todo_items=Todo_items_ref.current;
-        }
-      });
+import Button from "@mui/material/Button";
+import React, { useEffect,useState } from "react";
 
-  const {values,errors,touched,handleBlur,handleChange,handleSubmit}=useFormik({
-    initialValues:initialvalues,
-   
-    onSubmit:(values,action)=>{
-        if(!localStorage.getItem('TodoList')){
-            
-            let Arr=JSON.parse(localStorage.getItem('TodoList'))
-            console.log("🚀 ~ file: todolist.js:11 ~ Todolist ~ ̥:",values);
-            Arr.push(values.name)
-            localStorage.setItem("TodoList",JSON.stringify(Arr));
-        }else{
-            let Arr=[]
-            Arr=JSON.parse(localStorage.getItem('TodoList'))
-            console.log("🚀 ~ file: todolist.js:25 ~ Todolist ~ ̥:",values);
-            Arr.push(values.name)
-            localStorage.setItem("TodoList",JSON.stringify(Arr));
-            // Get_todo()
-        }
-    }
-  })
 
+export default function Todo() {
+  const [todos , setTodos] = useState(localStorage.getItem('todos')  || []);
+  const [newTodo , setNewTodo] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
   
-  const Get_todo=()=>{
-    if(localStorage.getItem('TodoList')){
-
-        Todo_items= JSON.parse(localStorage.getItem('TodoList'))
-        console.log("🚀 ~ file: todolist.js:35 ~ Todolist ~ ̥:",Todo_items)
-        document.getElementById('todo_items').innerHTML=Todo_items
-    }
+  const handleNewTodo = (e) => {
+    setNewTodo(e.target.value);
   }
-//   Get_todo()
-// const todo=(items)=>{
-//     return items.map((item)=>{ <li>{item.name}</li>
-//     })
-// }
-  console.log("values",values)
-    return (
-      <form action="post" onSubmit={handleSubmit} noValidate
-      autoComplete="off" >
-        {/* component="form" */}
-        <Box
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '40ch' },
-      }}
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        marginTop: '20px',
+  const handleAddTodo = (e) => {
+    if (newTodo !== '') {
+        setTodos([...todos, newTodo]);
+        setNewTodo('');
+        document.getElementById("err").innerHTML=""
 
-      }}
-      
-      noValidate
-      autoComplete="off"
-    >
+      }else {
+        document.getElementById("err").innerHTML="please Enter To DO"
+      }
+  }
 
-      {/* <div>
+
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+
+  return (
+
+
+      <Box
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "40ch" },
+        }}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "20px",
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        {/* <div>
        <label htmlFor="name">Name</label>
        <input type="text" id='name' name='name' placeholder='Enter name'/>
       </div> */}
-      <div>
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Enter Todo"
-          name='name'
-          type='text'
-          multiline
-          maxRows={4}
-          value={values.name}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          autoComplete='off'
-        />
-      </div>
-      <div style={{color:'red'}}>
-      {errors.name && touched.name ? errors.name : null}
-      </div>
-      
-      <div>
-      <Button variant="outlined" type='submit' style={{marginTop:'10px'}}>ADD</Button>
+        <div>
+        <FormControl>
+                        <TextField
+                          placeholder="Type something here…"
+                          minRows={3}
+                          onChange={handleNewTodo}
+                          sx={{
+                            minWidth: 300,
+                          }}
+                          value={newTodo}
+                        />
+                        <Button variant="contained" color="primary" sx={{ m: 2 }} onClick={handleAddTodo}>Add To Do</Button>
+                  </FormControl>
+                  <div id="err" style={{color:'red'}}>"Please Enter todo"</div>
+        </div>
+        {/* <div style={{ color: "red" }}>
+          {errors.name && touched.name ? errors.name : null}
+        </div> */}
 
-      </div>
-    </Box>
-    <div>
-    <p onLoad={Get_todo} id='todo_items'></p>
-    <p>{Todo_items_ref.current.map((name)=>{return <li>{name}</li>})}</p>
-    </div>
-       </form>
+        <div>
+          <Button
+            variant="outlined"
+            type="submit"
+            style={{ marginTop: "10px" }}
+          >
+            ADD
+          </Button>
+        </div>
     
-    )
+        <Box sx={{ width : '100%' , bgcolor : 'background.paper' }}>
+              <List
+                  sx={{ width: '100%' , bgcolor: 'background.paper' }}
+              >
+                  {todos.map((todo, index) => (
+                    <ListItem key={index} sx = {{ bgcolor : "#f5f5f5" }}>
+                      <ListItemText  primary={todo} >
+                           {todo}
+                      </ListItemText>
+                      <ListItemSecondaryAction>
+                        <IconButton edge="end" aria-label="delete" onClick={() => handleDeleteTodo(index)}>
+                          Delete
+                        </IconButton>
+                      </ListItemSecondaryAction>
+                    </ListItem>
+                  ))}
+              </List>
+               <Typography align="right" color="inherit" component="div" sx = {{margin : 2}}>
+                    {todos.length} To Do
+                </Typography>
+            </Box>      
+      </Box>      
+
+  );
 }
